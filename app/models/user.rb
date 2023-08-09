66beb1be0056feea73448e0d51f1bab756a4b1e1
HigeTurtle <higetaku45@gmail.com>
2023-08-09 20:34:13 +0900

    新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  valid_name = /\A[ぁ-んァ-ン一-龥]/
+  valid_name_kana = /\A[ァ-ヶー－]+\z/
+  valid_passeord = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  
+
+  validates :nickname, presence: true
+  validates :first_name, presence: true, format: { with: valid_name }
+  validates :last_name, presence: true, format: { with: valid_name }
+  validates :first_name_kana, presence: true, format: { with: valid_name_kana }
+  validates :last_name_kana, presence: true, format: { with: valid_name_kana }
+  validates :birthday, presence: true
+
+  
+  validates :password, format: { with: valid_passeord }
+
+  
+
 end
