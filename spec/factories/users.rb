bb00925d0ff4597dcb0667d359eb5bfcd93cf54e
HigeTurtle <higetaku45@gmail.com>
2023-08-12 10:11:46 +0900

    エラーメッセージの表示

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,12 +4,12 @@ FactoryBot.define do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.email}
     password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
+    password_confirmation { password }
     first_name            { Gimei.name.first.kanji }
     last_name             { Gimei.name.last.kanji }
     first_name_kana       { Gimei.name.first.katakana }
     last_name_kana        { Gimei.name.last.katakana }
     birthday              { Faker::Date.birthday }
-    
+
   end
 end
