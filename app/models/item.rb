581434638506bfe149d69792371fe28992494a7f
HigeTurtle <higetaku45@gmail.com>
2023-08-13 22:16:53 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,19 @@
 class Item < ApplicationRecord
   has_one_attached :image
-  
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :condition_id, numericality: { other_than: 1 } 
+  validates :delivery_charge_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :delivery_schedule_id, numericality: { other_than: 1 } 
+  validates :price, presence: true
+  validates :image, presence: true
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :condition, :delivery_charge, :prefecture, :delivery_schedule
+
+  
 end
