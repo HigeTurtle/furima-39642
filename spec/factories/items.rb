6a04c9b84d509b34af548f4a56e4fb6d661369da
HigeTurtle <higetaku45@gmail.com>
2023-08-15 02:29:14 +0900

    itemモデルの単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
-    
+
+    name               {Faker::Game.title}
+    explanation        {Faker::Lorem.sentence}
+    category_id        { 2 }
+    condition_id       { 2 }
+    charge_id          { 2 }
+    prefecture_id      { 2 }
+    schedule_id        { 2 }
+    price              {Faker::Number.between(from: 300, to: 9999999)}
+
+    association :user 
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/onepiece14_enel.png'), filename: 'onepiece14_enel.png')
+    end
+
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,81 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '商品出品できるとき' do
+      it '正しく商品出品できた' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品出品できないとき' do
+      # name バリデーション確認
+      it 'nameが空では登録できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+
+      # explanation バリデーション確認
+      it 'explanationが空では登録できない' do
+        @item.explanation = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      end
+
+      # activehash バリデーション確認
+      it 'category_idが「---」の時は保存できない登録できない' do
+        @item.category_id = '---'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'condition_idが「---」の時は保存できない登録できない' do
+        @item.condition_id = '---'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it 'charge_idが「---」の時は保存できない登録できない' do
+        @item.charge_id = '---'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Charge can't be blank")
+      end
+      it 'prefecture_idが「---」の時は保存できない登録できない' do
+        @item.prefecture_id = '---'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'schedule_idが「---」の時は保存できない登録できない' do
+        @item.schedule_id = '---'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Schedule can't be blank")
+      end
+
+      # image バリデーション確認
+      it 'imageが1枚でもないと保存できない登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'priceが空では登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが300未満では登録できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it 'priceが10000000以上では登録できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+    end
+  end
 end
+
