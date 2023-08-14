f1fb97b29c8898b4edc5e5efec1ca5a0d0d283e2
HigeTurtle <higetaku45@gmail.com>
2023-08-14 18:54:17 +0900

    商品情報の保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,22 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new ,:create]
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+      if @item.save
+        redirect_to root_path
+      else
+        render :new, status: :unprocessable_entity
+      end
+  end
+  
 
   private
-  def message_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:name, :image, :explanation, :category_id, :condition_id, :charge_id, :prefecture_id, :schedule_id, :price) .merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,22 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
-
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :charge
+  belongs_to :prefecture
+  belongs_to :schedule
+  
   validates :name, presence: true
   validates :explanation, presence: true
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :condition_id, numericality: { other_than: 1 } 
-  validates :charge_id, numericality: { other_than: 1 } 
-  validates :prefecture_id, numericality: { other_than: 1 } 
-  validates :schedule_id, numericality: { other_than: 1 } 
-  validates :price, presence: true
+  validates :category_id, numericality: { other_than: 1  , message: "can't be blank"} 
+  validates :condition_id, numericality: { other_than: 1  , message: "can't be blank"} 
+  validates :charge_id, numericality: { other_than: 1  , message: "can't be blank"} 
+  validates :prefecture_id, numericality: { other_than: 1  , message: "can't be blank"} 
+  validates :schedule_id, numericality: { other_than: 1  , message: "can't be blank"} 
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },format: { with: /\A[0-9]+\z/ }
   validates :image, presence: true
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :condition, :charge, :prefecture, :schedule
-
   
 end
