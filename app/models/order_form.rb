680b3f460f9fce66f83395c440e4d6c864b6a4c7
HigeTurtle <higetaku45@gmail.com>
2023-08-21 14:23:52 +0900

    （修正_2）address/orderモデルとコントローラー作成・編集

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -5,7 +5,7 @@
     validates :user    , presence: true
     validates :item    , presence: true
   
-    validates :post_code    , presence: true, format: /\A\d{3}[-]?\d{4}\z/ 
+    validates :post_code    , presence: true, format: /\A\d{3}[-]?\d{4}\z/ , format: /\A[0-9]+\z/
     validates :prefecture_id, numericality: { other_than: 1  , message: "can't be blank"} 
     validates :city         , presence: true
     validates :address      , presence: true
